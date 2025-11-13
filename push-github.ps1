e2f5bd37ab38c0da1e85339a4439df9232abf903
Tapia <JTQMEXIT@mabe.com.mx>
2025-11-13 11:24:07 -0600

    Primer commit

---
+++ push-github.ps1
@@ -0,0 +1,4 @@
+cd "C:\Users\JTQMEXIT\OneDrive - Controladora Mabe, S.A. de C.V\a_JT\mine\automation\QRTechnicalSpecifications"
+git add index.html
+git commit -m "Actualización automática desde Excel"
+git push origin main
